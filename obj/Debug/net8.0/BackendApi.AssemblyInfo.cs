680d34978b2d823f4ee7825f22b558a54bf4108c
ludkant <ludkant@gmail.com>
2025-05-23 11:44:44 -0500

    mysql
    conectando mysql

--- obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13b1ec188e70c1257782d4a3786418c9ead4cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
