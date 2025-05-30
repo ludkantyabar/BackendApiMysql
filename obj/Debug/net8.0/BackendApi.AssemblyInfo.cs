56a8d809fab2e038442db24e20024e6969eb4990
Ludkant Yabar Galindo <ludkant@gmail.com>
2025-05-30 14:58:26 -0500

    nmp install
    npm install

--- obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9b87576a8a37b34041703c1fe5fb6ba5aa2d71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd598fe3c9deb21af113e2747eaf18928a6d4088")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
