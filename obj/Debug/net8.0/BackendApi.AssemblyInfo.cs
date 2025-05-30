fd598fe3c9deb21af113e2747eaf18928a6d4088
Ludkant Yabar Galindo <ludkant@gmail.com>
2025-05-30 14:32:52 -0500

    cambios
    cambios

--- obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680d34978b2d823f4ee7825f22b558a54bf4108c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9b87576a8a37b34041703c1fe5fb6ba5aa2d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
